cb73f41065ebb262e0ad6012611c2998f61e6c97
asif <asifmansuri736@gmail.com>
2023-10-28 10:18:01 +0530

    completed

---
+++ src/data/index.js
@@ -0,0 +1,13 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Aasif',
+    lastName: 'Pinjari',
+    email: 'asifmansuri736@gmail.com',
+    salary: '20000',
+    date: '2023-08-30'
+  },
+  
+];
+
+export { employeesData };
